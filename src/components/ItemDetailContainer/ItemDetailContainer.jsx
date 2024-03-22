665c036027852fb3a8a4ae43ab3a35fbeb93bde2
ooldigital0 <ooldigital0@gmail.com>
2024-03-21 22:02:15 -0300

    item detail y useparams en itemdetailcontainer

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,6 +1,9 @@
 import'./ItemDetailContainer.css'
 import { useState, useEffect } from 'react'
 import {getProductById} from '../../../asyncMock'
+import {ItemDetail} from './ItemDetail'
+import { useParams } from 'react-router-dom'
+
 
 const ItemDetailContainer = () => {
     const [product, serProduct] = useState(null)
