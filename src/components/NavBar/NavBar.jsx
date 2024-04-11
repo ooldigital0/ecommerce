c1a765cdccb065fa72f1fa233cf97a76b2888168
ooldigital0 <ooldigital0@gmail.com>
2024-04-11 08:35:36 -0300

    modificaciones en el navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,3 +1,4 @@
+// NavBar.js
 import './NavBar.css'
 import CartWidget from "../CartWidget/CartWidget"
 import { NavLink, Link } from 'react-router-dom'
@@ -9,18 +10,10 @@ const NavBar = () => {
                 <h3>Infinita Serendipia</h3>
             </Link>
             <div className='Categories'>
-                <button className={({ isActive }) => isActive ? 'ActiveOption' : 'Option'}>
-                    <NavLink to={`/category/ebook`}>Guía Ayurveda</NavLink>
-                </button>
-                <button className={({ isActive }) => isActive ? 'ActiveOption' : 'Option'}>
-                    <NavLink to={`/category/ebook`}>Guía Astrología</NavLink>
-                </button>
-                <button className={({ isActive }) => isActive ? 'ActiveOption' : 'Option'}>
-                    <NavLink to={`/category/ebook`}>Guía Lunar</NavLink>
-                </button>
-                <button className={({ isActive }) => isActive ? 'ActiveOption' : 'Option'}>
-                    <NavLink to={`/category/ebook`}>Guía Tzolkin</NavLink>
-                </button>
+                <NavLink to="/category/ebook" activeClassName="active" exact>Guía Ayurveda</NavLink>
+                <NavLink to="/category/astrologia" activeClassName="active" exact>Guía Astrología</NavLink>
+                <NavLink to="/category/lunar" activeClassName="active" exact>Guía Lunar</NavLink>
+                <NavLink to="/category/tzolkin" activeClassName="active" exact>Guía Tzolkin</NavLink>
             </div>
             <CartWidget/>
             <span></span>
